5be750980935e029f7d62d21e0fa5a435a8c341a
Sparakala21 <paraks@rpi.edu>
2025-05-19 13:17:22 -0400

    remove reference to metadata

--- sravan-online/src/app/layout.tsx
+++ sravan-online/src/app/layout.tsx
@@ -1,5 +1,3 @@
-// RootLayout.tsx (server component)
-import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ThemeRegistry from "./ThemeRegistry";
