a607bd24dd9711c5fedfe0a095faab17e76bf887
Sparakala21 <paraks@rpi.edu>
2025-05-19 10:11:44 -0400

    use container

--- sravan-online/src/app/page.tsx
+++ sravan-online/src/app/page.tsx
@@ -1,8 +1,15 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
+import { Container } from '@mui/material';
 
 export default function Home() {
   return (
-     <Button variant="contained">press to press button</Button>
+     <Container>
+      <h1>Welcome to Sravan Online</h1>
+      <p>This is a sample application.</p>
+      <Button variant="contained" color="primary">
+        Click Me
+      </Button>
+     </Container>
   );
 }
