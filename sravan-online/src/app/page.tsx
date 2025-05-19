b5329ac4c4c1b1fda917b4b8d95fc9427b8ee23a
Sparakala21 <paraks@rpi.edu>
2025-05-19 14:32:54 -0400

    adding ResponsiveAppBar and using it in the main page

--- sravan-online/src/app/page.tsx
+++ sravan-online/src/app/page.tsx
@@ -2,52 +2,29 @@
 
 import * as React from 'react';
 import { Button, Container, Typography, Box } from '@mui/material';
+import ResponsiveAppBar from '@/components/ResponsiveAppBar'; // Importing the AppBar component
 
 export default function Home() {
   return (
-     <Container 
-       sx={{ 
-         bgcolor: '#f0ead6',
-         height: "100vh",
-         padding: 3,
-         position: 'relative', // Add this for absolute positioning context
-       }}
-     >
+    <>
+      <ResponsiveAppBar />
+      <Container 
+        sx={{ 
+          bgcolor: '#f0ead6',
+          minHeight: "calc(100vh - 64px)", // Adjust for AppBar height
+          padding: 3,
+          position: 'relative',
+        }}
+      >
         <Typography
           variant="h1"
           sx={{
             marginBottom: '20px',
+            paddingTop: 2, // Add some space at the top
           }}>
           Hello World
         </Typography>
-        
-        <Box sx={{ textAlign: 'right' }}>
-          <Button
-            variant="contained"
-            sx={{
-              backgroundColor: 'primary.main',
-              color: 'white',
-              '&:hover': {
-                backgroundColor: 'primary.dark',
-              },
-            }}
-          >
-            Resume
-          </Button>
-
-          <Button
-            variant="contained"
-            sx={{
-              backgroundColor: 'primary.main',
-              color: 'white',
-              '&:hover': {
-                backgroundColor: 'primary.dark',
-              },
-            }}
-          >
-            Contact me
-          </Button>
-        </Box>
-     </Container>
+      </Container>
+    </>
   );
 }
