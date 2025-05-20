812bb534a82760ede6a0b19ccb0d56712f070eaf
Sparakala21 <paraks@rpi.edu>
2025-05-20 11:03:50 -0400

    escape apostrophe

--- sravan-online/src/app/page.tsx
+++ sravan-online/src/app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
             align: 'center', // Center align text
           }}
         >
-          I'm Sravan
+          I&aposm Sravan
         </Typography>
         <Container
           sx={{
