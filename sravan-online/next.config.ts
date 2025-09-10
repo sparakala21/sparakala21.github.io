20f218d9095312ca89f5a26851a34b00ce05d957
Sparakala21 <paraks@rpi.edu>
2025-09-10 12:10:42 -0400

    config changes

--- sravan-online/next.config.ts
+++ sravan-online/next.config.ts
@@ -1,19 +1,14 @@
+//next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // basePath: "/sravan-online",
+  basePath: "/sravan-online",
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
   trailingSlash: true,
   images: {
     unoptimized: true
   },
-  compilerOptions: {
-    "moduleResolution": "bundler", // or "node"
-    "esModuleInterop": true,
-    "allowSyntheticDefaultImports": true
-  },
-  transpilePackages: ['next-mdx-remote/rsc'],
 };
 
 export default nextConfig;
