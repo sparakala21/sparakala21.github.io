3a9dc5347bc9ab5382df600120866b2f6ba6195b
Sparakala21 <paraks@rpi.edu>
2026-04-25 10:54:22 -0400

    swtich url

--- GTEx/main.js
+++ GTEx/main.js
@@ -1,4 +1,4 @@
-const API = 'http://localhost:8000';
+const API = 'https://api-production-f255.up.railway.app/';
 
 const TISSUES = [
   'adipose tissue', 'adrenal gland', 'amygdala', 'appendix', 'basal ganglia',
